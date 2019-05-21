6cd6ec098fcc139e5a19cf03235ef00a8d5e0444
Steven Huynh-Tran <htransteven@gmail.com>
2019-05-21 12:56:43 -0700

    Fixed responsiveness

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
-function init() {
+var timer = 0;
 
+function init() {
+  var invisObjs = $(".invis");
+  invisObjs.each(function() {
+    setTimeout(function(obj) {
+      $(obj).addClass("fadeIn");
+      $(obj).removeClass("invis");
+    }, timer, this);
+    timer += 300;
+  });
 }
