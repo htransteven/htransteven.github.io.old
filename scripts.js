ea5f4fe294a5aa0349a8a964957550852137ea09
Steven Huynh-Tran <htransteven@gmail.com>
2019-05-22 01:34:00 -0700

    Added more content and fade

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 var timer = 0;
 
 function init() {
+  window.scrollTo(0,0);
   var invisObjs = $(".invis");
   invisObjs.each(function() {
     setTimeout(function(obj) {
       $(obj).addClass("fadeIn");
       $(obj).removeClass("invis");
     }, timer, this);
+
     timer += 300;
   });
 }
