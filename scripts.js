3d9f508941472325032289f7bca68cd3bd89c066
Steven Huynh-Tran <htransteven@gmail.com>
2019-05-28 13:42:55 -0700

    Entry animation and custom scroll bar and fixed scrolling reset issue by hiding all elements at the beginning

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
 var timer = 0;
 
-function init() {
-  window.scrollTo(0,0);
+function displayNone() {
+  var aboutWrapper = $(".about-wrapper");
+  var schoolWrapper = $(".school-wrapper");
+  var expWrapper = $(".exp-wrapper");
+
+  aboutWrapper.hide();
+  schoolWrapper.hide();
+  expWrapper.each(function(){
+    $(this).hide();
+  });
+}
+
+function displayAll() {
+  var aboutWrapper = $(".about-wrapper");
+  var schoolWrapper = $(".school-wrapper");
+  var expWrapper = $(".exp-wrapper");
+
+  aboutWrapper.show();
+  schoolWrapper.show();
+  expWrapper.each(function(){
+    $(this).show();
+  });
+}
+
+function unHideAll(){
   var invisObjs = $(".invis");
   invisObjs.each(function() {
     setTimeout(function(obj) {
@@ -9,6 +32,34 @@ function init() {
       $(obj).removeClass("invis");
     }, timer, this);
 
-    timer += 200;
+    timer += 150;
   });
+}
+
+function entryAnimation() {
+  var titleObj = $(".title-wrapper");
+
+  //expand line
+  setTimeout(function(titleObj) {
+    titleObj.css("transition", "all 0.75s ease-in-out");
+    titleObj.toggleClass("line");
+  }, 500, titleObj);
+
+  //move to top
+  setTimeout(function(titleObj) {
+    titleObj.css("transition", "all 0.5s ease-in-out");
+    titleObj.css("top", "0");
+    titleObj.css("left", "50%");
+    titleObj.css("transform", "translateX(-50%)");
+  }, 1250, titleObj);
+
+  setTimeout(function() {
+    displayAll();
+    unHideAll();
+  }, 2000)
+}
+
+function init() {
+  displayNone();
+  entryAnimation();
 }
