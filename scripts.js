22d226e312e06cc5deb3ce70cffde3b1853bb268
Steven Huynh-Tran <htransteven@gmail.com>
2019-06-02 00:54:40 -0700

    Added menu button

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 var timer = 0;
+var menuOpen = false;
 
 function displayNone() {
   var aboutWrapper = $(".about-wrapper");
@@ -36,9 +37,55 @@ function unHideAll(){
   });
 }
 
-function entryAnimation() {
-  var titleObj = $(".title-wrapper");
+function loadMenuClickEvent(){
+  var titleArrow = $(".title-downarrow");
 
+  titleArrow.click(function(){
+    var titleObj = $(".title-wrapper");
+    var titleMenu = $(".title-menu-wrapper");
+    var menuItems = titleMenu.children();
+    var menuItemHeight = 25;
+    var extendMenuLength = menuItems.length * menuItemHeight;
+
+    if(!menuOpen){
+      let itemTimer = 0;
+      menuItems.each(function(){
+        setTimeout(function(item){
+          $(item).css("height", `${menuItemHeight}px`);
+          $(item).css("opacity", "1");
+        }, itemTimer, this);
+        itemTimer += 100;
+      });
+      menuOpen = true;
+
+    } else {
+      let itemTimer = 0;
+      $(menuItems.get().reverse()).each(function(){ //reverse fade order of items
+        setTimeout(function(item){
+          $(item).css("opacity", "0");
+          $(item).css("height", "0px");
+        }, itemTimer, this);
+        itemTimer += 100;
+      });
+      menuOpen = false;
+    }
+  });
+}
+
+function entryAnimation() {
+
+  loadMenuClickEvent();
+
+  //arrow bounce effect
+  var titleArrow = $(".title-downarrow");
+  setTimeout(function(titleArrow) {
+    titleArrow.css("transform", "translateY(7px)");
+  }, 250, titleArrow);
+  setTimeout(function(titleArrow) {
+    titleArrow.css("transform", "translateY(0px)");
+  }, 600, titleArrow);
+
+  var titleObj = $(".title-wrapper");
   //expand line
   setTimeout(function(titleObj) {
     titleObj.css("transition", "all 0.75s ease-in-out");
